32acbbf6c5d9b9c2b2b5719b96ffc15515777ec4
mdsobur0990-cmd <mdsobur0990@gmail.com>
2026-06-09 21:12:52 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
+const CACHE_NAME = 'pro-scanner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v1').then(cache => cache.addAll(['index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
